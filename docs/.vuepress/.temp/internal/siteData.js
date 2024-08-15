4b824b088321018547e85dbf59f45c8105a092d7
袁浩 <yuanhaozhuzhu@hotmail.com>
2024-08-15 12:36:05 +0800

    Deploy site

--- docs/.vuepress/.temp/internal/siteData.js
+++ docs/.vuepress/.temp/internal/siteData.js
@@ -1,1 +1,1 @@
-export const siteData = JSON.parse("{\"base\":\"/goravel-workflow/github.io/\",\"lang\":\"zh-CN\",\"title\":\"goravel-workflow\",\"description\":\"基于goravel的workflow审批流框架\",\"head\":[],\"locales\":{}}")
+export const siteData = JSON.parse("{\"base\":\"/\",\"lang\":\"zh-CN\",\"title\":\"goravel-workflow\",\"description\":\"基于goravel的workflow审批流框架\",\"head\":[],\"locales\":{}}")
--- docs/.vuepress/.temp/pages/contributing.html.js
+++ docs/.vuepress/.temp/pages/contributing.html.js
@@ -1,3 +1,3 @@
 import comp from "/Users/yh/code/Sites/goland/workflow/goravel-workflow-vuepress/docs/.vuepress/.temp/pages/contributing.html.vue"
-const data = JSON.parse("{\"path\":\"/contributing.html\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1723637210000,\"contributors\":[{\"name\":\"袁浩\",\"email\":\"yuanhaozhuzhu@hotmail.com\",\"commits\":1}]},\"filePathRelative\":\"contributing.md\"}")
+const data = JSON.parse("{\"path\":\"/contributing.html\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1723695774000,\"contributors\":[{\"name\":\"袁浩\",\"email\":\"yuanhaozhuzhu@hotmail.com\",\"commits\":1}]},\"filePathRelative\":\"contributing.md\"}")
 export { comp, data }
