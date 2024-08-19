a0e574a9a9846abff450124cfca878c43dccea4a
袁浩 <yuanhaozhuzhu@hotmail.com>
2024-08-19 18:30:36 +0800

    更新徽章

--- docs/.vuepress/.temp/pages/contributing.html.js
+++ docs/.vuepress/.temp/pages/contributing.html.js
@@ -1,16 +1,3 @@
 import comp from "/Users/yh/code/Sites/goland/workflow/goravel-workflow-vuepress/docs/.vuepress/.temp/pages/contributing.html.vue"
 const data = JSON.parse("{\"path\":\"/contributing.html\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[],\"git\":{\"updatedTime\":1723695774000,\"contributors\":[{\"name\":\"袁浩\",\"email\":\"yuanhaozhuzhu@hotmail.com\",\"commits\":1}]},\"filePathRelative\":\"contributing.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
--- docs/.vuepress/.temp/pages/guides/steps.html.js
+++ docs/.vuepress/.temp/pages/guides/steps.html.js
@@ -1,16 +1,3 @@
 import comp from "/Users/yh/code/Sites/goland/workflow/goravel-workflow-vuepress/docs/.vuepress/.temp/pages/guides/steps.html.vue"
-const data = JSON.parse("{\"path\":\"/guides/steps.html\",\"title\":\"使用步骤\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[{\"level\":3,\"title\":\"📖演示\",\"slug\":\"演示\",\"link\":\"#演示\",\"children\":[]},{\"level\":3,\"title\":\"⚒️定义模板\",\"slug\":\"定义模板\",\"link\":\"#定义模板\",\"children\":[]},{\"level\":3,\"title\":\"📝创建流程\",\"slug\":\"创建流程\",\"link\":\"#创建流程\",\"children\":[]},{\"level\":3,\"title\":\"🕹️设计流程\",\"slug\":\"设计流程\",\"link\":\"#设计流程\",\"children\":[]},{\"level\":3,\"title\":\"🎊保存发布\",\"slug\":\"保存发布\",\"link\":\"#保存发布\",\"children\":[]}],\"git\":{\"updatedTime\":1723958359000,\"contributors\":[{\"name\":\"袁浩\",\"email\":\"yuanhaozhuzhu@hotmail.com\",\"commits\":2}]},\"filePathRelative\":\"guides/steps.md\"}")
+const data = JSON.parse("{\"path\":\"/guides/steps.html\",\"title\":\"使用步骤\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[{\"level\":3,\"title\":\"📖演示\",\"slug\":\"演示\",\"link\":\"#演示\",\"children\":[]},{\"level\":3,\"title\":\"⚒️定义模板\",\"slug\":\"定义模板\",\"link\":\"#定义模板\",\"children\":[]},{\"level\":3,\"title\":\"📝创建流程\",\"slug\":\"创建流程\",\"link\":\"#创建流程\",\"children\":[]},{\"level\":3,\"title\":\"🕹️设计流程\",\"slug\":\"设计流程\",\"link\":\"#设计流程\",\"children\":[]},{\"level\":3,\"title\":\"🎊保存发布\",\"slug\":\"保存发布\",\"link\":\"#保存发布\",\"children\":[]}],\"git\":{\"updatedTime\":1724063136000,\"contributors\":[{\"name\":\"袁浩\",\"email\":\"yuanhaozhuzhu@hotmail.com\",\"commits\":3}]},\"filePathRelative\":\"guides/steps.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
