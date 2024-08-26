4dd1fda6e5029748862ab4adbc756d52acb691de
袁浩 <yuanhaozhuzhu@hotmail.com>
2024-08-26 12:16:04 +0800

    示意图

--- docs/.vuepress/.temp/pages/guides/api.html.js
+++ docs/.vuepress/.temp/pages/guides/api.html.js
@@ -1,16 +1,3 @@
 import comp from "/Users/yh/code/Sites/goland/workflow/goravel-workflow-vuepress/docs/.vuepress/.temp/pages/guides/api.html.vue"
 const data = JSON.parse("{\"path\":\"/guides/api.html\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{},\"headers\":[{\"level\":2,\"title\":\"API\",\"slug\":\"api\",\"link\":\"#api\",\"children\":[{\"level\":3,\"title\":\"1. 基础\",\"slug\":\"_1-基础\",\"link\":\"#_1-基础\",\"children\":[]},{\"level\":3,\"title\":\"2. 流程模板&模板字段\",\"slug\":\"_2-流程模板-模板字段\",\"link\":\"#_2-流程模板-模板字段\",\"children\":[]},{\"level\":3,\"title\":\"3. 流程发起\",\"slug\":\"_3-流程发起\",\"link\":\"#_3-流程发起\",\"children\":[]},{\"level\":3,\"title\":\"4. 流程处理&流转\",\"slug\":\"_4-流程处理-流转\",\"link\":\"#_4-流程处理-流转\",\"children\":[]},{\"level\":3,\"title\":\"5. 同意与驳回\",\"slug\":\"_5-同意与驳回\",\"link\":\"#_5-同意与驳回\",\"children\":[]}]}],\"git\":{\"updatedTime\":1724063136000,\"contributors\":[{\"name\":\"袁浩\",\"email\":\"yuanhaozhuzhu@hotmail.com\",\"commits\":3}]},\"filePathRelative\":\"guides/api.md\"}")
 export { comp, data }
-
-if (import.meta.webpackHot) {
-  import.meta.webpackHot.accept()
-  if (__VUE_HMR_RUNTIME__.updatePageData) {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  }
-}
-
-if (import.meta.hot) {
-  import.meta.hot.accept(({ data }) => {
-    __VUE_HMR_RUNTIME__.updatePageData(data)
-  })
-}
